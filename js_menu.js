ce7491f2f2d2653cc1c1485108ef2ac89b6685e5
maks2000-del <58049528+maks2000-del@users.noreply.github.com>
2020-11-12 22:50:48 +0300

    Add files via upload

--- js_menu.js
+++ js_menu.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').click(function (event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     })
+    
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+        
+        let elementID = $(this).data("scroll");
+        let elementOffset = $(elementID).offset().top;
+        
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 800);
+        
+    });
 });
