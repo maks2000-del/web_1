cfe0479b235ea29b900268132a2a4db9d6b66e5c
maks2000-del <58049528+maks2000-del@users.noreply.github.com>
2020-12-05 18:53:59 +0300

    Add files via upload

--- js_menu.js
+++ js_menu.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.header__burger,.header__menu').click(function (event) {
         $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
+        if (window.innerWidth < 767)
+            $('body').toggleClass('lock');
     })
 
     $('.close').click(function (event) {
@@ -9,19 +10,16 @@ $(document).ready(function(){
         $('.store__block, .store__buy__button, .store__clear__button, .store__field__button, .button__del, .button__store').toggleClass('lock');
     })
 
-    
+
     $("[data-scroll]").on("click", function (event) {
         event.preventDefault();
-        
+
         let elementID = $(this).data("scroll");
         let elementOffset = $(elementID).offset().top;
-        
+
         $("html, body").animate({
             scrollTop: elementOffset - 70
         }, 800);
-        
+
     });
 });
-
-
-    
